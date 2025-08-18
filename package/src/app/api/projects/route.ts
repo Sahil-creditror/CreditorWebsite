47d65e0f09c075905bbc708efdcbaa850d667eed
sahil <sahil@creditoracademy.com>
2025-08-18 17:05:56 +0530

    courses

--- package/src/app/api/projects/route.ts
+++ package/src/app/api/projects/route.ts
@@ -3,6 +3,6 @@ import { getAllProjects } from "@/lib/markdown";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const projects = getAllProjects(["title", "slug", "ScopeOfWork", "industry", "coverImage"]);
+  const projects = getAllProjects(["title", "slug", "ScopeOfWork", "industry", "coverImage", "description"]);
   return NextResponse.json(projects);
 }

aea345a170ea9a8b5da680dd5ffe2a7dc03c6ca9
sahil <sahil@creditoracademy.com>
2025-09-02 16:32:37 +0530

    button issue

--- package/src/app/components/operate/business/index.tsx
+++ package/src/app/components/operate/business/index.tsx
@@ -89,7 +89,7 @@ export default function InfographicTable({
   className = "",
   onRowClick,
   onCta,
-  ctaLabel = "Get Started",
+  ctaLabel = "Enroll Now",
 }: InfographicTableProps) {
   return (
     <section className={`${className} relative w-full px-4 sm:px-6 lg:px-8 py-16 sm:py-20 overflow-hidden`}>

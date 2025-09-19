ce41a200d182c52aa4961d45d02e541a17df79ea
Ankit Kumar <ankit@creditoracademy.com>
2025-09-18 22:18:33 -0700

    Updates

--- package/src/app/layout.tsx
+++ package/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 import ScrollToTop from "./components/scroll-to-top";
@@ -45,6 +46,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={manrope.className}>
+        {/* Microsoft Clarity Analytics */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "td1bkz5bto");
+            `,
+          }}
+        />
         <SessionProvider>
           <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
             {!hideLayout && <Header />}

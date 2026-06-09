a10ed9b28edb14f0002c2f76fce43800817c08bf
amaan-lms <amaan.javed@lmsathena.com>
2026-06-09 10:23:48 +0530

    nn

--- package/src/app/components/about/aboutus-detail/index.tsx
+++ package/src/app/components/about/aboutus-detail/index.tsx
@@ -236,14 +236,15 @@ const MovingBorder = ({
     }
   });
 
-  const x = useTransform(
-    progress,
-    (val) => pathRef.current?.getPointAtLength(val).x
-  );
-  const y = useTransform(
-    progress,
-    (val) => pathRef.current?.getPointAtLength(val).y
-  );
+  const x = useTransform(progress, (val) => {
+    const point = pathRef.current?.getPointAtLength(val);
+    return point?.x ?? 0;
+  });
+
+  const y = useTransform(progress, (val) => {
+    const point = pathRef.current?.getPointAtLength(val);
+    return point?.y ?? 0;
+  });
 
   const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
 

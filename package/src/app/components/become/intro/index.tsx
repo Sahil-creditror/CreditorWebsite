ef6559c335f1e4b5a87414f0991d41431151d620
amaan-lms <amaan.javed@lmsathena.com>
2026-07-24 14:39:40 +0530

    banner update

--- package/src/app/components/become/intro/index.tsx
+++ package/src/app/components/become/intro/index.tsx
@@ -117,7 +117,7 @@ export default function CourseOverviewSection() {
           <div className="w-full aspect-video rounded-2xl relative overflow-hidden">
             <iframe
               className="absolute inset-0 w-full h-full"
-              src="https://drive.google.com/file/d/1_zji-DvTUwqY-ss1l3HA3JBFwQgbNSQp/preview"
+              src="https://drive.google.com/file/d/1lVYIF9buDMaQkP5aoKQL6R9-6pbXq2D_/view?usp=drive_link"
               allow="autoplay"
               allowFullScreen
               title="Course overview video"
